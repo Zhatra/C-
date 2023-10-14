98dfb444051cb0bed3884da87cf8435673c6e8e8
Christian <chrtaw@outlook.com>
2023-10-14 00:39:09 -0600

    first hello word! with cpp

---
+++ helloword.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    //This is a comment
+    /*
+        This 
+        is 
+        a 
+        multi-line 
+        comment
+    */
+
+    std::cout << "I like bananas!" << '\n';
+    std::cout << "It's really good!" << '\n';
+    return 0;
+}
