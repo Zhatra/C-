522af9772b95d2f1e2fc8ec4fbca914abf70d702
Christian <chrtaw@outlook.com>
2023-10-14 19:04:14 -0600

    constants

--- helloword.cpp
+++ helloword.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(){
+    //const
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
     //integers (whole number)
     int x = 5;
     int y = 6;
